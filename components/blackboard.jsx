84ff3a87c3915bc7b4a4b1b1660fdcbcf3076907
wangly19 <18607948581@163.com>
2020-08-20 15:07:13 +0800

    合并组件动态数据

--- components/blackboard.jsx
+++ components/blackboard.jsx
@@ -1,10 +1,75 @@
 import React, { useEffect, useState } from 'react';
 import { Card, Space, Avatar, Tag } from 'antd';
 import { StarOutlined, BranchesOutlined, EyeOutlined } from '@ant-design/icons';
+import cheerio from 'cheerio';
+import rq from 'request-promise';
 import 'antd/dist/antd.css';
 import classes from '../public/styles/blackboard.css';
 
 export default props => {
+  const getRepoData = async ({ user, repo }) => {
+    const $ = await rq({
+      uri: `https://github.com/${user}/${repo}`,
+      transform: function(body) {
+        return cheerio.load(body, {
+          ignoreWhitespace: true,
+        });
+      },
+    });
+    const tags = [];
+    $('.topic-tag.topic-tag-link').each(function(i, elem) {
+      tags[i] = $(this)
+        .text()
+        .replace(/\r\n/g, '')
+        .replace(/\n/g, '')
+        .replace(/ /g, '');
+    });
+    return {
+      watching: $('ul.pagehead-actions > li:nth-child(1) a.social-count')
+        .text()
+        .replace(/\r\n/g, '')
+        .replace(/\n/g, '')
+        .replace(/ /g, ''),
+      stars: $('ul.pagehead-actions > li:nth-child(2) a.social-count')
+        .text()
+        .replace(/\r\n/g, '')
+        .replace(/\n/g, '')
+        .replace(/ /g, ''),
+      forks: $('ul.pagehead-actions > li:nth-child(2) a.social-count')
+        .text()
+        .replace(/\r\n/g, '')
+        .replace(/\n/g, '')
+        .replace(/ /g, ''),
+      description: $('p.f4.mt-3')
+        .text()
+        .replace(/\r\n/g, '')
+        .replace(/\n/g, '')
+        .replace(/ /g, ''),
+      tags: tags,
+    };
+  };
+  const getAvatar = async ({ user }) => {
+    const $ = await rq({
+      uri: `https://github.com/${user}`,
+      transform: function(body) {
+        return cheerio.load(body);
+      },
+    });
+    return $('img.TableObject-item.avatar').attr('src');
+  };
+
+  const [githubData, setGithubData] = useState({});
+  const [avatar, setAvatar] = useState('');
+  useEffect(() => {
+    getRepoData(props).then(data => {
+      setGithubData(data);
+    });
+    getAvatar(props).then(avatar => {
+      console.log('[blackboard]', avatar);
+      setAvatar(avatar);
+    });
+  }, []);
+
   return (
     <Card
       className={classes.githubCard}
@@ -27,9 +92,9 @@ export default props => {
       ]}
     >
       <Card.Meta
-        avatar={<Avatar size="large" src={props.avatar} />}
-        title={props.repo.title}
-        description={props.repo.description}
+        avatar={<Avatar size="large" src={avatar} />}
+        title={props.repo}
+        description={githubData.description}
       />
       <div className={classes.tagView}>
         {githubData.tags
@@ -49,8 +114,8 @@ export default props => {
 };
 
 /**
- * 图标文字组件
- * @param { Object } params 图标文字
+ *
+ * @param {  } param0
  */
 const IconText = ({ icon, text }) => (
   <Space>
--- docs/getting-started.en-US.md
+++ docs/getting-started.en-US.md
@@ -10,13 +10,5 @@ nav:
 import React from 'react';
 import Card from '../components/blackboard.jsx';
 
-export default () => (
-  <Card
-    avatar="https://avatars2.githubusercontent.com/u/12810740?s=200&v=4"
-    repo={{
-      title: 'element-ui',
-      description: 'A Vue.js 2.0 UI Toolkit for Web',
-    }}
-  />
-);
+export default () => <Card user="juejin-im" repo="open-source" />;
 ```
