ae2ec7b760a3ebbff1ec30ca46b1c75d0c9d5752
杨俊宁 <youngjuning@aliyun.com>
2020-08-28 01:19:41 +0800

    使用静态卡片

--- components/staticCard.jsx
+++ components/staticCard.jsx
@@ -6,42 +6,44 @@ import classes from '../public/styles/blackboard.css';
 
 const StaticCard = props => {
   return (
-    <Card
-      style={{ display: 'inline-block' }}
-      className={classes.githubCard}
-      actions={[
-        <IconText
-          icon={EyeOutlined}
-          text={props.repo.watching}
-          key="EyeOutlined"
-        />,
-        <IconText
-          icon={StarOutlined}
-          text={props.repo.stars}
-          key="StarOutlined"
-        />,
-        <IconText
-          icon={BranchesOutlined}
-          text={props.repo.fork}
-          key="BranchesOutlined"
-        />,
-      ]}
-    >
-      <Card.Meta
-        avatar={<Avatar size="large" src={props.repo.avatar} />}
-        title={props.repo.title}
-        description={props.repo.description}
-      />
-      <div className={classes.tagView}>
-        {props.tagList.map(item => {
-          return (
-            <Tag className={classes.tagLine} color="blue" key={item}>
-              {item}
-            </Tag>
-          );
-        })}
-      </div>
-    </Card>
+    <a href={`https://github.com/${props.repo.title}`} target="_blank">
+      <Card
+        style={{ display: 'inline-block' }}
+        className={classes.githubCard}
+        actions={[
+          <IconText
+            icon={EyeOutlined}
+            text={props.repo.watching}
+            key="EyeOutlined"
+          />,
+          <IconText
+            icon={StarOutlined}
+            text={props.repo.stars}
+            key="StarOutlined"
+          />,
+          <IconText
+            icon={BranchesOutlined}
+            text={props.repo.fork}
+            key="BranchesOutlined"
+          />,
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar size="large" src={props.repo.avatar} />}
+          title={props.repo.title}
+          description={props.repo.description}
+        />
+        <div className={classes.tagView}>
+          {props.repo.tagList.map(item => {
+            return (
+              <Tag className={classes.tagLine} color="blue" key={item}>
+                {item}
+              </Tag>
+            );
+          })}
+        </div>
+      </Card>
+    </a>
   );
 };
 
--- docs/card/index.zh-CN.md
+++ docs/card/index.zh-CN.md
@@ -7,16 +7,7 @@ nav:
 
 ```jsx | inline
 import React from 'react';
-import Card from '../../components/blackboard.jsx';
-
-export default () => {
-  return <Card repo="juejin-im/open-source" />;
-};
-```
-
-```jsx | inline
-import React from 'react';
-import Card from '../../components/blackboard.jsx';
+import Card from '../../components/staticCard.jsx';
 
 export default () => {
   return (
@@ -27,10 +18,17 @@ export default () => {
         flexWrap: 'wrap',
       }}
     >
-      <Card repo="juejin-im/open-source" />
-      <Card repo="juejin-im/open-source" />
-      <Card repo="juejin-im/open-source" />
-      <Card repo="juejin-im/open-source" />
+      <Card
+        repo={{
+          watching: 17,
+          stars: 205,
+          fork: 16,
+          avatar: 'https://avatars3.githubusercontent.com/u/69633008?s=200&v=4',
+          title: 'juejin-im/open-source',
+          description: '🏆 寻找你最值得关注的开源库',
+          tagList: ['juejin', 'open-source'],
+        }}
+      />
     </div>
   );
 };
